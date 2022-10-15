8fe398492d4b7a472af5f856b85f4ba3558b3735
SLFireFLash <sllahiruprasanna@gmail.com>
2022-10-15 21:18:32 +0530

    set world complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const words =["Hello","phone","watch"];
 var score = document.getElementById("score-display");
+var row_1 =[document.getElementById("box-1-1"),document.getElementById("box-1-2"),document.getElementById("box-1-3"),document.getElementById("box-1-4"),document.getElementById("box-1-5")];
+
 
 
 function randNum(){
@@ -9,6 +11,11 @@ function randNum(){
 
 function setWord(){
     var randWord =words[randNum()];
-    score.innerHTML =randWord;
+    for(let a=0;a <randWord.length;a++)
+    {
+        
+        row_1[a].innerHTML =randWord.charAt(a);
+        console.log(row_1[a] + "." + randWord.charAt(a) );
+    }
 }
 
