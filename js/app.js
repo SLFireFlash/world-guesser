1c9dc8db3d5b97ad5e6e0b0392d35b4a92868c0a
SLFireFLash <sllahiruprasanna@gmail.com>
2022-10-16 10:21:27 +0530

    First Row Done

--- js/app.js
+++ js/app.js
@@ -1,21 +1,40 @@
-const words =["Hello","phone","watch"];
+const words =["HELLO","PHONE","WATCH"];
+const startGame =document.getElementById("StartGame");
 var score = document.getElementById("score-display");
-var row_1 =[document.getElementById("box-1-1"),document.getElementById("box-1-2"),document.getElementById("box-1-3"),document.getElementById("box-1-4"),document.getElementById("box-1-5")];
+var row_1 =[document.getElementById("box-value-1-1"),document.getElementById("box-value-1-2"),document.getElementById("box-value-1-3"),document.getElementById("box-value-1-4"),document.getElementById("box-value-1-5")];
 
 
-
-function randNum(){
-    var Number =Math.floor(Math.random()*3);
-    return Number;
+function randWordGen(){
+    var randWord =words[Math.floor(Math.random()*3)];
+    return randWord;
 }
 
 function setWord(){
-    var randWord =words[randNum()];
-    for(let a=0;a <randWord.length;a++)
+    var gameWord = randWordGen();
+    var hidePos = Math.floor(Math.random()*3)
+    console.log("hide pos: " + hidePos)
+    for(let a=hidePos;a <3;a++)
     {
-        
-        row_1[a].innerHTML =randWord.charAt(a);
-        console.log(row_1[a] + "." + randWord.charAt(a) );
+            row_1[a].innerHTML =gameWord.charAt(a);
+            row_1[a].style.backgroundColor = "green"
+            row_1[a].style.color = "white"
+            console.log(row_1[a] + "." + gameWord.charAt(a) );
+
     }
+    if(hidePos !=0){
+        for(var b =0;b<hidePos;b++)
+        {
+            row_1[b].innerHTML ="?";
+            row_1[b].style.backgroundColor ="red";
+        }
+    }
+    row_1[3].innerHTML ="?";
+    row_1[3].style.backgroundColor ="red";
+    row_1[4].innerHTML ="?";
+    row_1[4].style.backgroundColor ="red";
+
+
+    //startGame.setAttribute("disabled","");
 }
+
 
